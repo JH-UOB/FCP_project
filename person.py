9b499da7fa46843c22e1850f75c0bca7ceedef43
JHawke <jameshawke56@gmail.com>
2021-03-29 15:51:09 +0100

    Added attributes to person.py

--- person.py
+++ person.py
@@ -7,25 +7,35 @@ Created on Sun Mar 28 16:31:32 2021
 
 import numpy as np
 import random
+# from task import get_task
 
-class person(params):
-    
-    def __init__(self):
-        self.age = random.randint(params['Maximum Age'], params['Minimum Age'])
+class person():
+
+    def __init__(self, params):
+        self.age = random.randint(params['Minimum Age'], params['Maximum Age'])
         if random.random() > 0.5:
             self.gender = 'Male'
         else:
             self.gender = 'Female'
-            
-        if random.random < params['Mask likelihood']:
+        if random.random() < params['Mask adherance']:
             self.mask = True
         else:
             self.mask = False
-            
-        if random.random < params['Social distancing likelihood']:
+        if random.random() < params['Social distancing likelihood']:
             self.social_distancing = True
         else:
             self.social_distancing = False
-        
     
-        
+        self.desk_location = (4,10)
+        self.task_location = self.desk_location.copy()
+        self.current_location = self.desk_location.copy()
+        self.task_duration = random.randint(1, 10)
+        self.task_progress = 0
+        self.doing_task = True
+
+
+params = {'Maximum Age' : 65,
+          'Minimum Age' : 18,
+          'Mask adherance' : 0.8,
+          'Social distancing likelihood' : 0.5}
+jim = person(params)

f57d035c0db8b33a40950f03b32a88b4b88d0dc6
ajh2 <ajhonnywill@gmail.com>
2021-03-31 22:25:09 +0100

    Modified: Formatting.

--- office.py
+++ office.py
@@ -7,8 +7,10 @@ class Office:
     def __init__(self):
         self.display_array = pd.read_excel('office_array.xls').values.transpose()
         self.pathfinding_array = np.where(self.display_array != 0, 1, self.display_array)
-        self.desk_locations = list(zip(list(np.where(self.display_array == 'D'))[0], list(np.where(self.display_array == 'D'))[1]))
-        self.task_locations = list(zip(list(np.where(self.display_array == 'T'))[0], list(np.where(self.display_array == 'T'))[1]))
+        self.desk_locations = list(zip(list(np.where(self.display_array == 'D'))[0],
+                                       list(np.where(self.display_array == 'D'))[1]))
+        self.task_locations = list(zip(list(np.where(self.display_array == 'T'))[0],
+                                       list(np.where(self.display_array == 'T'))[1]))
         self.people = []  # to be populated
 
     # def detect_interactions(self):

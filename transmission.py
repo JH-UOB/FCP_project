5bb5390f0243bc28336687361fe35721b1890b42
Alex Straw <as17163@bristol.ac.uk>
2021-04-06 20:58:02 +0000

    transmission file now outputs the infected state of the very first person of the first interaction of the step

--- transmission.py
+++ transmission.py
@@ -8,7 +8,27 @@ Description:
     The person class is updated to reflect any changes made.
 """
 
+
 def do_something(people,person,interactions):
-    print(len(interactions))
+
+    if len(interactions) > 0:
+        person_1_ID = abs(int(interactions[0][0]))
+        print(people[person_1_ID].infected)
+    else:
+        pass
+    """
+    for i in range(0, len(interactions)):
+        person_1_ID = abs(int(interactions[i][0]))
+        person_2_ID = interactions[i][1]
+        distance = interactions[i][2]
+
+
+        print(person_1_ID)
+        print(person_2_ID)
+        print(distance)
+        print("")
+    """
+
+
 
 

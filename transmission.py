fb7a7cac33891a35a03dd916b35642be4bf86155
Alex Straw <as17163@bristol.ac.uk>
2021-04-06 20:25:41 +0000

    prints the number of interactions --> prototyping to show that import step is working

--- transmission.py
+++ transmission.py
@@ -1,3 +1,14 @@
-import pandas as pd
-import numpy as np
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 28 16:31:32 2021
+
+Author: Alex Straw
+Description:
+    Takes all relevant office interactions and determines if an infection of COVID-19 has taken place.
+    The person class is updated to reflect any changes made.
+"""
+
+def do_something(people,person,interactions):
+    print(len(interactions))
+
 

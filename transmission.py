b33cbee3ae24774bb9148644d4bd9a9a85e5f4c2
Alex Straw <as17163@bristol.ac.uk>
2021-04-06 21:36:26 +0000

    interactions checked for each step and contagious interactions are output where only 1 person has COVID-19

--- transmission.py
+++ transmission.py
@@ -7,16 +7,34 @@ Description:
     Takes all relevant office interactions and determines if an infection of COVID-19 has taken place.
     The person class is updated to reflect any changes made.
 """
+def get_contagious_interactions(people,interactions):
+    contagious_interactions = []
 
+    for i in range(0, len(interactions)):
+        person_1_ID = abs(int(interactions[i][0])) - 1  #Acquiring IDs for people involved in interaction i of current step
+        person_2_ID = abs(int(interactions[i][1])) - 1
+
+        if people[person_1_ID].infected != people[person_2_ID].infected:
+            distance = interactions[i][2] #find distance apart if interaction is relevant
+            contagious_interactions.extend((person_1_ID,person_2_ID,distance))
+
+    return(contagious_interactions)
 
 def do_something(people,person,interactions):
 
+    if len(interactions) > 0:
+        contagious_interactions = get_contagious_interactions(people,interactions)
+        print(contagious_interactions)
+    else:
+        pass
+
+    """
     if len(interactions) > 0:
         person_1_ID = abs(int(interactions[0][0]))
         print(people[person_1_ID].infected)
     else:
         pass
-    """
+
     for i in range(0, len(interactions)):
         person_1_ID = abs(int(interactions[i][0]))
         person_2_ID = interactions[i][1]

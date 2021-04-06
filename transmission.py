2a979a2dc50cabbbdf91fd18cdb74e9fa6bc8d56
Alex Straw <as17163@bristol.ac.uk>
2021-04-06 21:45:45 +0000

    cleaned up file by removing unnecessary comments

--- transmission.py
+++ transmission.py
@@ -31,25 +31,4 @@ def do_something(people,person,interactions):
 
 
 
-    """
-    if len(interactions) > 0:
-        person_1_ID = abs(int(interactions[0][0]))
-        print(people[person_1_ID].infected)
-    else:
-        pass
-
-    for i in range(0, len(interactions)):
-        person_1_ID = abs(int(interactions[i][0]))
-        person_2_ID = interactions[i][1]
-        distance = interactions[i][2]
-
-
-        print(person_1_ID)
-        print(person_2_ID)
-        print(distance)
-        print("")
-    """
-
-
-
 

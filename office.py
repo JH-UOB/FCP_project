a4658765b8cb21d5264ed17506f278d89b1a3eff
ajh2 <ajhonnywill@gmail.com>
2021-04-15 01:15:32 +0100

    Added comments for class functionality and updated formatting.

--- office.py
+++ office.py
@@ -1,8 +1,41 @@
+"""
+
+office.py
+Designed and written by Adam Honnywill and James Hawke, predominantly through "pair coding"
+April 2021
+
+This script contains a class that defines an office space and processes it for display and movement purposes.
+
+Used by simulation.py.
+"""
+
 import pandas as pd
 import numpy as np
 from scipy.spatial import distance
 
+
 class Office:
+    """This class is used to generate arrays that track the locations of tasks, desks and people as the latter move
+    through the simulation. To do this, it has three key purposes:
+
+        1. Generating pathfinding arrays
+
+        Pathfinding arrays are processed from an .xls floorplan and input into the simulation by the office class.
+        They need to be formatted such that any traversable cell has a value greater than 0: wall cells have a value
+        of 0; floor, task and desk sitting cells have a value of 1, and people occupied cells have a value that is
+        equal to their negative ID number, to simultaneously track their position and make their location not
+        traversable to others.
+
+        2. Display arrays - TBC
+
+        Display arrays are processed pathfinding arrays to be displayed correctly.
+
+        3. Detecting interactions
+
+        As the pathfinding array stores the locations of people, these can be analysed at each time step to detect
+        interactions that might lead to transmission.
+
+    """
 
     def __init__(self):
         """
@@ -12,7 +45,7 @@ class Office:
         """
         # Create an array using the excel input file
         self.display_array = pd.read_excel('office_array.xls').values.transpose()
-        # Create pathfinding array denoting which cells are transversible.
+        # Create pathfinding array denoting which cells are traversible.
         self.pathfinding_array = np.where(self.display_array != 0, 1, self.display_array)
         # Find locations of tasks and desks
         self.desk_locations = list(zip(list(np.where(self.display_array == 'D'))[0],
@@ -25,9 +58,10 @@ class Office:
         # Create people locations dictionary to be populated and updated as 
         # people move.
         self.people_locations = {}
-        
-        
+
     def adj_finder(self, matrix, position, interactions=False):
+        """Used to detect if cells adjacent to the one occupied are available for moving into. The optional
+        interactions flag adds a social """
         adj = []
 
         for dx in range(-1, 2):
@@ -35,50 +69,54 @@ class Office:
                 rangeX = range(0, matrix.shape[0])  # X bounds
                 rangeY = range(0, matrix.shape[1])  # Y bounds
 
-                (newX, newY) = (position[0]+dx, position[1]+dy)  # adjacent cell
+                (newX, newY) = (position[0] + dx, position[1] + dy)  # adjacent cell
 
                 if (newX in rangeX) and (newY in rangeY) and (dx, dy) != (0, 0):
+                    # If within office walls and not itself...
                     if interactions:
+                        # ... and not a wall, select cells as adjacent
                         if matrix[newX, newY] != 0:
                             adj.append((newX, newY))
                     else:
+                        # ... and not a person or a wall, select cells as adjacent
+                        # NOTE: cells occupied by people are represented by a value equal to their negative id
                         if matrix[newX, newY] > 0:
                             adj.append((newX, newY))
         return adj
 
     def find_interactions(self, matrix, person_loc):
+        """Return a list of interactions that includes the people interacting and the distance between them"""
+        # Find person adjacent cells that are unoccupied by walls
         adj_cells = self.adj_finder(matrix, person_loc, True)
         extra_cells = []
         for cell in adj_cells:
+            # Find cells adjacent to person adjacent cells i.e. to detect wall unoccupied cells within 2 cells of the
+            # person
             extra_cells.extend(self.adj_finder(matrix, cell, True))
         adj_cells.extend(extra_cells)
-        adj_cells = [x for x in adj_cells if x != person_loc]
+        adj_cells = [x for x in adj_cells if x != person_loc]  # filters cell to remove person location
         interactions = []
         for cell in adj_cells:
             if matrix[cell] < 0:
+                # Checks if adjacent cells are occupied by people and records their relative distance in an interaction
+                # if so
                 interactions.append([max(matrix[person_loc], matrix[cell]),
-                                         min(matrix[person_loc], matrix[cell]),
-                                              distance.euclidean(person_loc, cell)])
+                                     min(matrix[person_loc], matrix[cell]),
+                                     distance.euclidean(person_loc, cell)])
         return interactions
 
+################################################### Change to read in people list I think?
+
     def fill_social_distancing_array(self, current_person_ID, people_locations):
+        """Returns a modified pathfinding array that enlarges the not traversable space around other people"""
         people = list(people_locations.keys())
         people.remove(current_person_ID)
         social_dist_array = self.pathfinding_array.copy()
         for person in people:
             bubble = self.adj_finder(self.pathfinding_array, people_locations[person])
             for location in bubble:
+                # Modifies pathfinding array to have 'walls' in cells adjacent to other people, so socially distant
+                # pathfinding keeps distant
                 social_dist_array[location] = 0
-                
+
         return social_dist_array
-
-
-
-
-
-
-
-
-
-
-
--- simulation.py
+++ simulation.py
@@ -2,8 +2,7 @@
 """
 
 simulation.py
-Adam Honnywill
-James Hawke
+Designed and written by Adam Honnywill and James Hawke, predominantly through "pair coding"
 April 2021
 
 This script runs simulations of coronavirus transmission in an office to investigate the effects of various
@@ -14,19 +13,24 @@ parameters, such as wearing or social distancing. The script may be used to:
     3. Show a plot of different stages of the epidemic
     4. Save a plot to a file
 
-This is done using code in this script, which uses uses classes in other scripts:
+This is done using code in this script, which in turn uses uses classes in other scripts:
 
     1. office.py            # to track the movement of people at any given time in the office space
     2. person.py            # to instantiate people in the office
     3. GUI.py               # to perform GUI based parameter input
     4. transmission.py      # to update the infection status of people upon interaction
 
-The command line interface to the script makes allows for user input parameters to be input either from a GUI or a
-text file:
+The command line interface to the script allows for user input parameters to be input either from a GUI or a
+text file, such that no code needs changing between simulations:
 
     $ python simulation.py               # run simulation with with text file input parameters
     $ python simulation.py --GUI         # run simulation with with GUI input parameters
     $ python simulation.py --help        # show all command line options
+
+In order to run this script, one or two input files must be present in the directory:
+
+    1. office_array.xls     # always
+    2. input_parameters.txt # if the --GUI flag is not called
 
 It is also possible to create a video of the animation (if you install
 ffmpeg):
@@ -41,7 +45,7 @@ import itertools
 import timeit
 import random
 import matplotlib.pyplot as plt
-from scipy.spatial import distance
+# from scipy.spatial import distance
 # from joblib import Parallel, delayed
 from person import Person
 from office import Office
@@ -64,9 +68,9 @@ def main():
                   'default_transmission_chance': 0.02
                   }
 
-    selected_office = Office()  # Initialise office space
-    selected_people = instantiate_people(parameters, selected_office)  # Initialise people in office space
-    run_simulation(parameters, selected_office, selected_people)
+    selected_office = Office()  # initialise office space
+    selected_people = instantiate_people(parameters, selected_office)  # initialise people in office space
+    run_simulation(parameters, selected_office, selected_people)  # run the simulation
     return selected_office
 
 
@@ -79,8 +83,8 @@ def instantiate_people(params, office):
     for ID in range(1, number_of_people + 1):
         people.append(Person(ID, office.desk_locations, params))
         # Update dictionary of people locations stored in Office object
-        office.people_locations[ID] = (people[ID - 1].current_location)
-        # Set person location in pathfinding array to be untraversable
+        office.people_locations[ID] = people[ID - 1].current_location
+        # Set person location in pathfinding array to be not traversable
         set_array_value(people[ID - 1].current_location[0], 
                         people[ID - 1].current_location[1],
                         office.pathfinding_array, - ID)
@@ -89,52 +93,67 @@ def instantiate_people(params, office):
     return people
 
 
-def update_location(people, person, office):
+def update_location(person, office):
+    """Moves people that are moving between their desk and their next task, attempting to socially distance only if
+    possible """
     set_array_value(person.current_location[0], person.current_location[1], office.pathfinding_array, 1)
 
     if person.social_distancing:
+        # Generate array for pathfinding whilst socially distancing
         social_dist_array = office.fill_social_distancing_array(person.ID, office.people_locations)
+        # Generate path through array
+        # NOTE: when path generation fails, it returns an empty list
         path = person.get_path(social_dist_array)
         if len(path) > 0:
+            # Socially distanced path generation successful, move along path
             person.move(path)
         else:
+            # Find path without socially distancing
             path = person.get_path(office.pathfinding_array)
             if len(path) > 0:
+                # Path without socially distancing generation successful, move along path
                 person.move(path)
             else:
+                # No path available, move avoid blockages
                 move_somewhere(person, office)
     else:
+        # Generate path through array
+        # NOTE: when path generation fails, it returns an empty list
         path = person.get_path(office.pathfinding_array)
     if len(path) > 0:
+        # Path without socially distancing generation successful, move along path and
         person.move(path)
-        set_array_value(person.current_location[0], person.current_location[1], office.pathfinding_array, - person.ID)
     else:
+        # No path available, move to avoid blockages
         move_somewhere(person, office)
-    set_array_value(person.current_location[0], 
+
+    # Update pathfinding array with person location for other people to navigate
+    set_array_value(person.current_location[0],
                     person.current_location[1], 
                     office.pathfinding_array, - person.ID)
-    office.people_locations[person.ID] = person.current_location
+    office.people_locations[person.ID] = person.current_location  # Question - why do we record people locations?
 
 
 def set_array_value(x, y, array, value):
+    """Updates an array"""
     array[x][y] = value
 
 
-def start_moving(people, person, office):
+def start_moving(person, office):
     """Assign a task to a person and start moving"""
     person.task_progress = 0
     # Assign a task
     person.get_task(office.task_locations)
     # Begin movement along path
-    update_location(people, person, office)
+    update_location(person, office)
 
 
 def move_somewhere(person, office):
-    """Move person somewhere to avoid blockages in narrow spaces"""
-    # Get available cells that can be moved into i.e. not a wall or another person
+    """Move person somewhere adjacent to avoid blockages in narrow spaces"""
+    # Get available, adjacent cells that can be moved into i.e. not a wall or another person
     avail_cells = office.adj_finder(office.pathfinding_array, 
                                     person.current_location)
-    # Set previous person location in pathfinding array to be traversable
+    # Set current person location in pathfinding array to be traversable
     set_array_value(person.current_location[0], 
                     person.current_location[1], 
                     office.display_array, 1)
@@ -143,15 +162,19 @@ def move_somewhere(person, office):
 
 
 def record_interactions(office, people):
+    """Checks for interactions in the office and stores them to simulate transmissions"""
     interactions = []
+    # Detect interactions between people
     for person in people:
         interactions.extend(office.find_interactions(office.pathfinding_array, person.current_location))
+    # Remove duplicate interactions
     interactions.sort()
     interactions = list(interactions for interactions, _ in itertools.groupby(interactions))
     return interactions
 
 
 def plot_figure(time, office):
+    """Plots the locations of people in the office as their locations are updated"""
     plt.figure(time)
     plt.title(str(time))
     plt.imshow(office.pathfinding_array.tolist())
@@ -173,36 +196,43 @@ def updated_infected(params, people, person, interactions, infection_debug):
 
 
 def run_simulation(params, office, people):
+    """Core sequence of logic of the simulation. Formatted to record results in 'frames' for each time step.
+
+    In each time step, everyone will progress a task, finish a task and move towards another task, or carry on moving
+    between tasks. If they come into contact with each other they will potentially transmit coronavirus.
+
+    """
+
+    # Initialise lists to record results
     sim_duration = params['Simulation Duration']
     display_frames = []  # used to store locations for each time tick, for running through in GUI
     people_frames = []  # used to store people states for each time tick, for running through in GUI
     office.interaction_frames = []
+
+    # For each time step, perform actions for each person in office
     for time in range(sim_duration):
-        # contents could be shifted to person class
         for person in people:  # move people as necessary
             if person.current_location == person.task_location:
-                if person.task_progress < person.task_duration:
+                if person.task_progress < person.task_duration:  # task incomplete, keep doing task
                     person.task_progress += 1
 
                 else:  # task complete, find new task and start moving
-                    start_moving(people, person, office)
+                    start_moving(person, office)
 
             else:  # between tasks, keep moving
-                update_location(people, person, office)
+                update_location(person, office)
 
-        print(time)
+        print(time)  # for tracking progress
         office.interactions = record_interactions(office, people)
-        office.interaction_frames.append(office.interactions)
+        office.interaction_frames.append(office.interactions)  # record interactions
 
         """     Infection & transmissibility    """
 
         infection_debug = True  # Used for testing, True will print data to log, False will pass function
         updated_infected(params, people, person, office.interactions, infection_debug)  # Update who is infected
 
-
-
-        display_frames.append(office.display_array)
-        people_frames.append(people)
+        display_frames.append(office.display_array)  # record people locations in office
+        people_frames.append(people)  # record status of people (included infection status)
         plot_figure(time, office)
 
 
